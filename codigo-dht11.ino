d8e148f19255acce8ac5a0180f2562679e140cd2
Ian Monteiro <ianmonteiromoreira@gmail.com>
2024-04-17 16:10:55 -0300

    Update codigo-dht11.ino

--- codigo-dht11.ino
+++ codigo-dht11.ino
@@ -2,10 +2,15 @@
 #include <LiquidCrystal.h>
 #include <DHT.h>
 
-// Definindo o pino do DHT, o tipo do DHT e o número de leituras para o cálculo da média
+// Definindo o pino do DHT, o tipo do DHT, o número de leituras para o cálculo da média e os pinos dos LEDs
 #define DHTPIN 7     // Pino de dados do DHT22 conectado ao pino digital 7
 #define DHTTYPE DHT11   // Tipo do sensor DHT
 #define NUM_READINGS 10  // Número de leituras para calcular a média
+#define LEDG 13 // Define o pino 13 digital para o led VERDE
+#define LEDY 8 // Define o pino 8 digital para o led AMARELO
+#define LEDR 6 // Define o pino 6 digital para o led VERMELHO
+#define BUZZER 9 // Define o pino digital 9 para a buzina
+#define DELAY_PADRAO 5000 // Define o delay padrão para 5 segundos
 
 DHT dht(DHTPIN, DHTTYPE);
 LiquidCrystal lcd(12, 11, 10, 5, 4, 3, 2);
@@ -20,6 +25,12 @@ void setup()
   // Iniciando o serial
   Serial.begin(9600);
   Serial.println("D A T A   S P H E R E");
+
+  // Definindo os pinos dos leds como SAÍDA
+  pinMode(LEDG, OUTPUT);   // Define o pino LEDG como saída
+  pinMode(LEDY, OUTPUT);   // Define o pino LEDY como saída
+  pinMode(LEDR, OUTPUT);   // Define o pino LEDR como saída
+  pinMode(BUZZER, OUTPUT); // Define o pino BUZZER como saída
 
   // Iniciando o DHT e o Display 16 x 2
   dht.begin();
@@ -38,7 +49,7 @@ void setup()
   lcd.print("S P H E R E");
 
   // Delay para o logo da empresa aparecer no display por 5 segundos
-  delay(5000);
+  delay(DELAY_PADRAO);
 }
 
 void loop() {
@@ -68,19 +79,55 @@ void loop() {
   lcd.print("Umidade");
 
   // Verifica o valor da média da UMIDADE e mostra no serial e no display
+  // Se a umidade estiver abaixo de 50%
   if (humidity < 50) {
+
+    // Desliga os outros LEDs e liga o vermelho, indicando estado crítico
+    digitalWrite(LEDG, LOW); // Desliga o LED verde
+    digitalWrite(LEDY, LOW);  // Desliga o LED amarelo
+    digitalWrite(LEDR, HIGH);  // Liga o LED vermelho
+
+    tone(BUZZER, 2000, 5000); // Dispara a buzina a uma frequência DE 2000Hz por 5 segundos
+
     Serial.print("Umidade BAIXA: ");
+
+    // Escreve no display no estado da umidade
     lcd.setCursor(0, 1);
     lcd.print("BAIXA");
+
+  // Se a umidade estiver entre 50% e 70%
   } else if (humidity >= 50 && humidity <= 70) {
+
+    // Desliga os outros LEDs e liga o verde, indicando que está OK
+    digitalWrite(LEDG, HIGH); // Liga o LED verde
+    digitalWrite(LEDY, LOW);  // Desliga o LED amarelo
+    digitalWrite(LEDR, LOW);  // Desliga o LED vermelho
+
     Serial.print("Umidade OK: ");
+
+    // Escreve no display no estado da umidade
     lcd.setCursor(0, 1);
     lcd.print("OK");
+
+  // Se a umidade estiver acima de 70%
   } else if (humidity > 70) {
+
+    // Desliga os outros LEDs e liga o vermelho, indicando estado crítico
+    digitalWrite(LEDG, LOW); // Desliga o LED verde
+    digitalWrite(LEDY, LOW);  // Desliga o LED amarelo
+    digitalWrite(LEDR, HIGH);  // Liga o LED vermelho
+
+    tone(BUZZER, 2000, 5000); // Dispara a buzina a uma frequência DE 2000Hz por 5 segundos
+
     Serial.print("Umidade ALTA: ");
+
+    // Escreve no display no estado da umidade
     lcd.setCursor(0, 1);
     lcd.print("ALTA");
+
   }
+
+  // Mostra a porcentagem de umidade no Serial e no Display
   Serial.print(humidity);
   Serial.println(" %\t");
   lcd.setCursor(13,0);
@@ -89,28 +136,56 @@ void loop() {
   lcd.print("%");
 
   // Delay para mostrar a umidade no display por 5 segundos
-  delay(5000);
+  delay(DELAY_PADRAO);
   
-  // 
-  // delay(NUM_READINGS * 1000);
   lcd.clear();
   lcd.setCursor(0, 0);
   lcd.print("Temperatura");
 
   // Verifica o valor da média da TEMPERATURA e mostra no serial e no display
   if (temperature < 10) {
+
+    // Desliga os outros LEDs e liga o amarelo, indicando estado de alerta
+    digitalWrite(LEDG, LOW); // Desliga o LED verde
+    digitalWrite(LEDY, HIGH);  // Liga o LED amarelo
+    digitalWrite(LEDR, LOW);  // Desliga o LED vermelho
+
+    tone(BUZZER, 1500, 5000); // Dispara a buzina a uma frequência DE 1500Hz por 5 segundos
+
     Serial.print("Temperatura BAIXA: ");
+
+    // Escreve no display no estado da temperatura
     lcd.setCursor(0, 1);
     lcd.print("BAIXA");
   } else if (temperature >= 10 && temperature <= 15) {
+
+    // Desliga os outros LEDs e liga o verde, indicando que está OK
+    digitalWrite(LEDG, HIGH); // Liga o LED verde
+    digitalWrite(LEDY, LOW);  // Desliga o LED amarelo
+    digitalWrite(LEDR, LOW);  // Desliga o LED vermelho
+
     Serial.print("Temperatura OK: ");
+
+    // Escreve no display no estado da temperatura
     lcd.setCursor(0, 1);
     lcd.print("OK");
   } else if (temperature > 15) {
+
+    // Desliga os outros LEDs e liga o amarelo, indicando estado de alerta
+    digitalWrite(LEDG, LOW); // Desliga o LED verde
+    digitalWrite(LEDY, HIGH);  // Liga o LED amarelo
+    digitalWrite(LEDR, LOW);  // Desliga o LED vermelho
+
+    tone(BUZZER, 1500, 5000); // Dispara a buzina a uma frequência DE 1500Hz por 5 segundos
+
     Serial.print("Temperatura ALTA: ");
+
+    // Escreve no display no estado da temperatura
     lcd.setCursor(0, 1);
     lcd.print("ALTA");
   }
+
+  // Mostra a temperatura em graus celsius no Serial e no Display
   Serial.print(temperature);
   Serial.println(" °C");
   lcd.setCursor(13,0);
@@ -119,7 +194,7 @@ void loop() {
   lcd.print("C");
 
   // Delay para mostrar a temperatura no display por 5 segundos
-  delay(5000);
+  delay(DELAY_PADRAO);
 
 
   /* 
